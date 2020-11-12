db3f9bc4fcf9f207df5c0bee1c71d2f7fa5219e4
Jonas Leijzen <jonasleijzen@gmail.com>
2020-11-12 22:41:04 +0100

    Added NetworkStorage.cs frame.

---
+++ MusicAI/NetworkStorage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace MusicAI.Storage {
+	public static class NetworkStorage {
+
+		public static Network Load (string path) {
+			if (!File.Exists (path)) {
+				Console.WriteLine ("Could load network because the file location does not exist.");
+
+			}
+			return new Network (0, 0, 0, 0);
+		}
+
+		public static void Save (Network network, string path) {
+			if (network == null) {
+				Console.WriteLine ("Could not write network because it doesn't exist.");
+				return;
+			}
+		}
+	}
+}
