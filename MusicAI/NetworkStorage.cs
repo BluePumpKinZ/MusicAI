456577a1f85d4ad0482475e6ceee28c9f74a6d43
Jonas Leijzen <jonasleijzen@gmail.com>
2020-11-12 22:44:32 +0100

    Changed Console.Writeline to proper errors.

--- MusicAI/NetworkStorage.cs
+++ MusicAI/NetworkStorage.cs
@@ -5,18 +5,14 @@ namespace MusicAI.Storage {
 	public static class NetworkStorage {
 
 		public static Network Load (string path) {
-			if (!File.Exists (path)) {
-				Console.WriteLine ("Could load network because the file location does not exist.");
-
-			}
+			if (!File.Exists (path))
+				throw new FileNotFoundException ("Could load network because the file location does not exist.", path);
 			return new Network (0, 0, 0, 0);
 		}
 
 		public static void Save (Network network, string path) {
-			if (network == null) {
-				Console.WriteLine ("Could not write network because it doesn't exist.");
-				return;
-			}
+			if (network == null)
+				throw new NullReferenceException ("Could not write network because it doesn't exist.");
 		}
 	}
 }
