66012fe3e9931276d5a6319a16bc7eedd6818544
Jonas Leijzen <jonasleijzen@gmail.com>
2020-11-12 21:40:06 +0100

    Added basic 3 dimensional vector class.

---
+++ MusicAI/Vector3.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace MusicAI.Vectors {
+	public class Vector3 {
+
+		public double x;
+		public double y;
+		public double z;
+
+		public Vector3 () => new Vector3 (0, 0, 0);
+
+		public Vector3 (double x, double y) => new Vector3 (x, y, 0);
+
+		public Vector3 (double x, double y, double z) {
+			this.x = x;
+			this.y = y;
+			this.z = z;
+		}
+
+		public static Vector3 operator + (Vector3 a, Vector3 b) {
+			return new Vector3 (a.x + b.x, a.y + b.y, a.z + b.z);
+		}
+
+		public static Vector3 operator - (Vector3 a, Vector3 b) {
+			return new Vector3 (a.x - b.x, a.y - b.y, a.z - b.z);
+		}
+
+		public static Vector3 operator * (Vector3 a, Vector3 b) {
+			return new Vector3 (a.x * b.x, a.y * b.y, a.z * b.z);
+		}
+
+		public static Vector3 operator / (Vector3 a, Vector3 b) {
+			return new Vector3 (a.x / b.x, a.y / b.y, a.z / b.z);
+		}
+
+		public static Vector3 operator + (double a, Vector3 b) {
+			return new Vector3 (a + b.x, a + b.y, a + b.z);
+		}
+
+		public static Vector3 operator - (double a, Vector3 b) {
+			return new Vector3 (a - b.x, a - b.y, a - b.z);
+		}
+
+		public static Vector3 operator * (double a, Vector3 b) {
+			return new Vector3 (a * b.x, a * b.y, a * b.z);
+		}
+
+		public static Vector3 operator / (double a, Vector3 b) {
+			return new Vector3 (a / b.x, a / b.y, a / b.z);
+		}
+
+		public static Vector3 operator + (Vector3 a, double b) {
+			return new Vector3 (a.x + b, a.y + b, a.z + b);
+		}
+
+		public static Vector3 operator - (Vector3 a, double b) {
+			return new Vector3 (a.x - b, a.y - b, a.z - b);
+		}
+
+		public static Vector3 operator * (Vector3 a, double b) {
+			return new Vector3 (a.x * b, a.y * b, a.z * b);
+		}
+
+		public static Vector3 operator / (Vector3 a, double b) {
+			return new Vector3 (a.x / b, a.y / b, a.z / b);
+		}
+
+		public double magnitude () {
+			return Math.Sqrt (sqrMagnitude ());
+		}
+
+		public double sqrMagnitude () {
+			return x * x + y * y + z * z;
+		}
+
+	}
+}
